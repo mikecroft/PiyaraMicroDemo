0f7a98020ab848668403fb7c5fefb2cbbc94ced8
Mike Croft <backtotheoldhouse@gmail.com>
2017-04-11 21:28:47 +0100

    added async rest sse client

--- StockMicroprofile/src/main/java/fish/payara/examples/payaramicro/stockmicroprofile/StockResource.java
+++ StockMicroprofile/src/main/java/fish/payara/examples/payaramicro/stockmicroprofile/StockResource.java
@@ -9,6 +9,7 @@ import fish.payara.examples.payaramicro.stockticker.Stock;
 import fish.payara.micro.cdi.ClusteredCDIEventBus;
 import fish.payara.micro.cdi.Inbound;
 import javax.enterprise.context.ApplicationScoped;
+import javax.enterprise.context.Destroyed;
 import javax.enterprise.context.Initialized;
 import javax.enterprise.event.Observes;
 import javax.inject.Inject;
@@ -20,6 +21,8 @@ import javax.ws.rs.client.ClientBuilder;
 import javax.ws.rs.client.WebTarget;
 import javax.ws.rs.core.MediaType;
 import org.glassfish.jersey.media.sse.EventInput;
+import org.glassfish.jersey.media.sse.EventListener;
+import org.glassfish.jersey.media.sse.EventSource;
 import org.glassfish.jersey.media.sse.InboundEvent;
 import org.glassfish.jersey.media.sse.SseFeature;
 
@@ -35,8 +38,11 @@ public class StockResource {
 
     @Inject
     private ClusteredCDIEventBus bus;
+    
+    private EventSource eventSource;
 
     public void init(@Observes @Initialized(ApplicationScoped.class) Object init) {
+        this.eventSource = openEventSource();
         bus.initialize();
     }
 
@@ -49,26 +55,30 @@ public class StockResource {
     public void observer(@Observes @Inbound Stock stock) {
         this.stock = stock;
     }
+    
+    public EventSource openEventSource() {
 
-    public void restObserver() {
-        
         //see https://jersey.java.net/documentation/latest/sse.html#d0e11986
-        
-        Client client = ClientBuilder.newBuilder()
-                .register(SseFeature.class).build();
+        Client client = ClientBuilder.newBuilder().register(SseFeature.class).build();
         // explicitly listen on 9999 to avoid autobindhttp nonsense
-        WebTarget target = client.target("http://localhost:9999/rest/sse");
+        WebTarget target = client.target("http://localhost:9999/StockWeb-1.0-SNAPSHOT/rest/sse");
 
-        EventInput eventInput = target.request().get(EventInput.class);
-        while (!eventInput.isClosed()) {
-            final InboundEvent inboundEvent = eventInput.read();
-            if (inboundEvent == null) {
-                // connection has been closed
-                break;
+
+        EventSource eventSource = EventSource.target(target).build();
+        EventListener listener = new EventListener() {
+            @Override
+            public void onEvent(InboundEvent inboundEvent) {
+                System.out.println(inboundEvent.getName() + "; "
+                        + inboundEvent.readData(String.class));
             }
-            System.out.println(inboundEvent.getName() + "; "
-                    + inboundEvent.readData(String.class));
-        }
+        };   
+        eventSource.register(listener, "stock-update");
+        eventSource.open();
+        return eventSource;
     }
 
+    
+    public void destroy(@Observes @Destroyed(ApplicationScoped.class) Object init) {
+        this.eventSource.close();
+    }
 }
