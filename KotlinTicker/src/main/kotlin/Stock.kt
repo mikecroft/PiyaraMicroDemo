ae24bf0a7c4742f431a93e587fd694cff325e2c2
Mike Croft <mikecroft@users.noreply.github.com>
2017-04-26 08:30:03 +0100

    Update Stock.kt
    tidy up

--- KotlinTicker/src/main/kotlin/Stock.kt
+++ KotlinTicker/src/main/kotlin/Stock.kt
@@ -2,14 +2,8 @@ import java.io.Serializable
 
 data class Stock(val symbol: String, val description: String, val price: Double) : Serializable {
 
-//    var symbol: String = "NOTHING"
-//    var description: String = "Uninitialised Stock object"
-//    var price: Double = 0.0
-
     override fun toString(): String {
         return "{" + "\"price\":" + price + ",\"symbol\":\"" + symbol + "\"}"
 
-
     }
-
 }
